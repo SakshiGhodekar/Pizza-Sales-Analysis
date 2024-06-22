3ed731c439c97d3e4336e6e0077414c3fae0800f
Sakshi Ghodekar <98404934+SakshiGhodekar@users.noreply.github.com>
2024-06-22 19:37:21 +0530

    Add files via upload

---
+++ 1.sql
@@ -0,0 +1,36 @@
+show databases;
+use pizzahut;
+create table orders(
+order_id int not null,
+order_date date not null,
+order_time time not null,
+primary key(order_id)
+);
+create table order_details(
+order_details_id int not null,
+order_id int not null,
+pizza_id text not null,
+quantity int not null,
+primary key(order_details_id)
+);
+
+
+-- Basic:
+-- Retrieve the total number of orders placed.
+-- Calculate the total revenue generated from pizza sales.
+-- Identify the highest-priced pizza.
+-- Identify the most common pizza size ordered.
+-- List the top 5 most ordered pizza types along with their quantities.
+
+
+-- Intermediate:
+-- Join the necessary tables to find the total quantity of each pizza category ordered.
+-- Determine the distribution of orders by hour of the day.
+-- Join relevant tables to find the category-wise distribution of pizzas.
+-- Group the orders by date and calculate the average number of pizzas ordered per day.
+-- Determine the top 3 most ordered pizza types based on revenue.
+
+-- Advanced:
+-- Calculate the percentage contribution of each pizza type to total revenue.
+-- Analyze the cumulative revenue generated over time.
+-- Determine the top 3 most ordered pizza types based on revenue for each pizza category. --
